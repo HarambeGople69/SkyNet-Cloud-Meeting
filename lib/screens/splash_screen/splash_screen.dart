e203a17f7f4e0a6fa5bb331b856dd6960c29dc0b
Utsav Shrestha <tsvshrsth@gmail.com>
2022-01-19 10:52:40 +0545

    getx added

--- lib/screens/splash_screen/splash_screen.dart
+++ lib/screens/splash_screen/splash_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -10,11 +13,15 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          "Splash Screen",
-        ),
+    return ScreenUtilInit(
+      builder: () => GetMaterialApp(
+        // initialBinding: MyBinding(),
+        useInheritedMediaQuery: true,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark),
+        // ignore: prefer_const_constructors
+
+        home: const SplashScreen(),
       ),
     );
   }
